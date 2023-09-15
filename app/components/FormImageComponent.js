d5398aa7f882440d45e024fc879786adbd002ee0
Ali Nasir <alee.nasir254@gmail.com>
2023-09-15 06:26:51 +0500

    third commit

---
+++ app/components/FormImageComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+function FormImageComponent(props) {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default FormImageComponent;
